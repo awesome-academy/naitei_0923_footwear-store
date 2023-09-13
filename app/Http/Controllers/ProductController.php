c19beac639f7e10cf7076c55dfbb698d22c12ce9
vandanghuy120385 <van.dang.huy@sun-asterisk.com>
2023-09-13 22:18:10 +0700

    design homepage

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::join('product_in_stocks', 'products.id', '=', 'product_in_stocks.product_id')
+            ->join('product_media', 'products.id', '=', 'product_media.product_id')
+            ->select(['products.name', 'price', 'products.id', 'brand','media_link'])
+            ->get();
+        $commeShoes = Product::join('product_in_stocks', 'products.id', '=', 'product_in_stocks.product_id')
+            ->join('product_media', 'products.id', '=', 'product_media.product_id')
+            ->select(['products.name', 'price', 'products.id', 'brand','media_link'])
+            ->where('brand', '=', 'Comme des Garcons')
+            ->get();
+            
+        return view('welcome', [
+            'products' => $products,
+            'commeShoes' => $commeShoes,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
